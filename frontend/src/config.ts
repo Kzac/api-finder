9649f267a5096cadcdb37300f842f9af15093d5d
kzacs <kevin@MacBook-Pro-de-Zac.local>
2024-12-15 17:24:54 +0100

    Update backend API URL in frontend config

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,6 @@
 const config = {
   apiBaseUrl: process.env.NODE_ENV === 'production' 
-    ? 'https://api-finder.vercel.app'
+    ? 'https://api-finder-j9ra-2lezj0n0u-kzacs-projects.vercel.app'
     : 'http://localhost:5001'
 };
 
